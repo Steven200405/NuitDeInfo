a302ef2808da8d360cf0d6abb52decd44e0c49e1
Steven200405 <huangsteven2004@gmail.com>
2024-12-06 06:28:29 +0100

    test

--- vignette/vignette.js
+++ vignette/vignette.js
@@ -16,7 +16,7 @@ const vignettes = {
         "Le Microbiome de l’océan : Essentiel à notre Santé et à l'Équilibre de la Planète",
         "Nos estomacs abrite des milliers de bonnes bactéries, champignons, virus et parasites non pathogènes. C’est ce que l’on appelle le microbiote. Il joue un rôle essentiel dans la digestion, la protection contre les infections, le maintien de l'équilibre du système immunitaire et même l'influence sur l'humeur et la santé mentale.L’océan est le microbiote de notre monde, il contient un très grand nombre de bactéries, de microbes, de virus, de champignons et de micro-algues. On l'appelle le microbiome, cette environnement permet à énormément d'espèce de se développer et à maintenire l’équilibre du monde animal. ",
         "Texte plus simple",
-        "./Images/microbiome.jpg",
+        "microbiome.jpg",
         "Description",
         "https://fondationtaraocean.org/mission-microbiomes/microbiome-marin-majorite-invisible-locean/"
     ),
