5c0dfe9099a4366b4872b89cdad527e8c168ba54
Cartier Lou <lou.cartier09@gmail.com>
2024-12-06 03:06:20 +0100

    Toutes les Vignettes

--- src/vignette/vignette.js
+++ src/vignette/vignette.js
@@ -1,4 +1,4 @@
-// Classe Vignette
+
 class Vignette {
     constructor(titre, texte, texteAccessible, image, descriptionImage, lien) {
         this.titre = titre;
@@ -10,7 +10,7 @@ class Vignette {
     }
 }
 
-// Création d'une instance de Vignette
+
 const Estomac = new Vignette(
     "Le Microbiome de l’océan : Essentiel à notre Santé et à l'Équilibre de la Planète",
     "Nos estomacs abrite des milliers de bonnes bactéries, champignons, virus et parasites non pathogènes. C’est ce que l’on appelle le microbiote. Il joue un rôle essentiel dans la digestion, la protection contre les infections, le maintien de l'équilibre du système immunitaire et même l'influence sur l'humeur et la santé mentale.L’océan est le microbiote de notre monde, il contient un très grand nombre de bactéries, de microbes, de virus, de champignons et de micro-algues. On l'appelle le microbiome, cette environnement permet à énormément d'espèce de se développer et à maintenire l’équilibre du monde animal. ",
@@ -28,10 +28,54 @@ const Peau = new Vignette(
     null
 );
 
+const Reins = new Vignette(
+    "Les Courants Océaniques et les Reins : Deux Forces de Redistribution et d'Élimination",
+    "Les courants de profondeur effectuent l’élimination et redistribution : ils transportent les eaux riches en nutriments et éliminent les déchets organiques des couches superficielles, tout comme les reins qui éliminent les déchets du sang tout en distribuant des substances essentielles.",
+    "Texte plus simple",
+    null,
+    "Description",
+    "Les Courants Océaniques : Différents Types, Causes, Effets Et Importance | Durabl"
+);
+
+const Poumons = new Vignette(
+    "Poumons et Océan : La Biodiversité, Gardienne  de la santé",
+    "Les poumons abritent une flore microbienne spécifique qui aide à la protection et à la santé respiratoire. L'océan, avec ses récifs coralliens et sa biodiversité marine, abrite une vie complexe qui soutient les cycles naturels, et permet à l’océan de capturer le CO2 et à distribuer l’oxygène. La biodiversité permet, tout comme la flore microbienne des poumons, de protéger les d'océan d'espèce invasive qui nuisent au bon équilibre mondiale.",
+    "Texte plus simple",
+    null,
+    "Description",
+    null
+);
+
+const Sang = new Vignette(
+    "Océan et Sang : La Circulation des Fluides au Service de la Température Planétaire",
+    "L’océan permet de de stocker la chaleur, régulant la température mondiale. Cela s'explique par les courants océaniques qui distribuent l’excès de chaleur reçu aux tropiques vers les plus hautes latitudes. D’une manière générale, les gaz sont plus solubles dans les eaux froides que chaudes. Par conséquent, les eaux de surfaces des hautes latitudes sont plus riches en gaz que celles des basses latitudes. Le renouvellement des eaux de surface par la circulation océanique jouent par conséquent un rôle très important dans le cycle du gaz carbonique et du carbone, en entraînant les eaux enrichies des hautes latitudes vers l’océan profond.",
+    "Texte plus simple",
+    null,
+    "Description",
+    "https://ocean-climate.org/presentation-des-fiches-scientifiques-ocean-et-climat/locean-reservoir-de-chaleur/"
+);
+
+const Coeur = new Vignette(
+    "L’océan :  Notre source d'Oxygène ",
+    "Le cœur permet de réoxygéner tout notre système sanguin à chaque battement tout comme les phytoplanctons (micro-organismes marins) , contenue dans l’océan, qui rejettent entre 50% à 80% d'oxygène dans notre atmosphère. De plus, l'océan absorbe un quart du CO2 rejeté par l’homme. ",
+    "Texte plus simple",
+    null,
+    "Description",
+    null
+);
+
+
+
 
 // Stockage de l'instance dans l'objet global
 window.Estomac = Estomac;
 window.Peau=Peau;
+window.Reins=Reins;
+window.Poumons=Poumons;
+window.Sang=Sang;
+window.Coeur=Coeur;
+
+
 
 // Fonction pour afficher le titre
 function afficherTout() {
